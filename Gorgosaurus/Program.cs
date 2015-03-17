b972dff86da16a9611a98b9b0373844cd3c4b8b2
Arthur <arthur.peka@outlook.com>
2015-03-18 00:23:15 +0200

    initial commit

---
+++ Gorgosaurus/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Gorgosaurus
+{
+    using System;
+    using Nancy.Hosting.Self;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uri =
+                new Uri("http://localhost:8080");
+
+            HostConfiguration hostConfigs = new HostConfiguration()
+            {
+                UrlReservations = new UrlReservations() { CreateAutomatically = true }
+            };
+
+            using (var host = new NancyHost(uri, new Bootstrapper(), hostConfigs))
+            {
+                host.Start();
+
+                Console.WriteLine("Your application is running on " + uri);
+                Console.WriteLine("Press any [Enter] to close the host.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Gorgosaurus/TopicModule.cs
@@ -0,0 +1,21 @@
+﻿using Gorgosaurus.BO.Entities;
+using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gorgosaurus
+{
+    public class TopicModule : NancyModule
+    {
+        public TopicModule()
+        {
+            Get["/topic/{id:int}"] = parameters => 
+            {
+                var resPost = new ForumPost() { Text = "Hello,"+parameters.id };
+                return Response.AsJson<ForumPost>(resPost);
+            };
+        }
+    }
+}
