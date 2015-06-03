6aff5c7e10944d371efa7c49985fec154ca2abba
Arthur <arthur.peka@outlook.com>
2015-06-03 22:08:56 +0300

    Using John Papa's style

--- Gorgosaurus/Content/Angular/forum.controller.js
+++ Gorgosaurus/Content/Angular/forum.controller.js
@@ -1,15 +1,17 @@
 ﻿(function () {
     angular.module('forumApp').controller('forumController', forumController);
 
-    forumController.$inject = ['$scope'];
+    forumController.$inject = ['$scope', '$http'];
 
-    function forumController($scope) {
+    function forumController($scope, $http) {
         var self = this;
 
-        $scope.posts = [
+        self.posts = [
             { id: 1, postText: "Haha" },
             { id: 5, postText: "Test" }
         ];
+
+
     }
 
 })();
