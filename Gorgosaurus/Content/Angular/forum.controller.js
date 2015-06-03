e96f8897c3e105fde21178bbb978d7117e93aead
Arthur <arthur.peka@outlook.com>
2015-06-03 21:38:12 +0300

    Starting to implement front-end part

---
+++ Gorgosaurus/Content/Angular/forum.controller.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    angular.module('forumApp').controller('forumController', forumController);
+
+    forumController.$inject = ['$scope'];
+
+    function forumController($scope) {
+        var self = this;
+
+        $scope.posts = [
+            { id: 1, postText: "Haha" },
+            { id: 5, postText: "Test" }
+        ];
+    }
+
+})();
