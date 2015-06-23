420a8e8e97ef948cdcc4d72e45f5cd9f6c460429
arthur-peka <arthur.peka@outlook.com>
2015-06-23 22:08:05 +0300

    Navbar mockup

--- Gorgosaurus/Program.cs
+++ Gorgosaurus/Program.cs
@@ -36,7 +36,8 @@
                 });
                 DiscussionRepository.Instance.Insert(new Discussion() { 
                     Id = 1, Title = "Hello!", SubforumId = 1, CreatedOnUnix = DateTime.UtcNow.ToUnixTimestamp() });
-                ForumPostRepository.Instance.Insert(new ForumPost() { Id = 1, PostText = "crap", DiscussionId = 1 });
+                var post = new ForumPost() { Id = 1, PostText = "crap", DiscussionId = 1 };
+                ForumPostRepository.Instance.Insert(post);
                 var ent = ForumPostRepository.Instance.Get(1);
 
                 Console.WriteLine("Your application is running on " + uri);
@@ -47,9 +48,25 @@
 
                 var entNew = ForumPostRepository.Instance.Get(1);
 
-                var allDiscussions = SubforumRepository.Instance.Get(1);
+                SubforumRepository.Instance.Insert(new Subforum()
+                {
+                    Title = null,
+                    Description = "talking about important stuff"
+                });
 
-                Console.ReadLine();
+                var allDiscussions = SubforumRepository.Instance.GetAll();
+
+                UserRepository.Instance.Insert(new ForumUser()
+                {
+                    Id = 1,
+                    Username = "lala",
+                    Password = "haha",
+                    IsUserAdmin = true
+                });
+
+                var user = UserRepository.Instance.Get(1);
+
+               Console.ReadLine();
             }
         }
     }
