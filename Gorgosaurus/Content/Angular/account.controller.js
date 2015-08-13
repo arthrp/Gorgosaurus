8f01e3d3aafd088a5024bad6dafaaec36cd703ab
Arthur <arthur.peka@outlook.com>
2015-08-14 00:17:41 +0300

    Starting to implement user concept

---
+++ Gorgosaurus/Content/Angular/account.controller.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    angular.module('forumApp').controller('accountController', accountController);
+
+    accountController.$inject = ['$scope', '$http'];
+
+    function accountController($scope, $http) {
+        var self = this;
+
+        self.greetingText = "";
+
+        self.getCurrentUser = function () {
+            $http.get("/account/current").success(function (resp) {
+                if (!resp) {
+                    self.greetingText = "Hello, guest";
+                }
+                else {
+                    self.greetingText = "Hello, " + resp;
+                }
+
+                console.log(self.greetingText);
+            });
+        }
+
+        self.getCurrentUser();
+    }
+})();
---
+++ Gorgosaurus/Modules/AccountModule.cs
@@ -0,0 +1,59 @@
+﻿using Gorgosaurus.Models;
+using Nancy;
+using Nancy.Cookies;
+using Nancy.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gorgosaurus.Modules
+{
+    public class AccountModule : NancyModule
+    {
+        private const string AUTH_COOKIE_NAME = "Grg_user";
+
+        public AccountModule()
+        {
+            Post["account/login"] = parameters =>
+            {
+                var loginModel = this.Bind<LoginModel>();
+
+                var response = new Response() { StatusCode = HttpStatusCode.OK };
+                response.Cookies.Add(GenerateCookie(loginModel.Username, DateTime.Now.AddMonths(3)));
+
+                return response;
+            };
+
+            Post["account/logout"] = parameters =>
+            {
+                var response = new Response() { StatusCode = HttpStatusCode.OK };
+
+                var requestCookie = Request.Cookies.FirstOrDefault(c => c.Key == AUTH_COOKIE_NAME);
+                string userName = requestCookie.Value;
+
+                if (String.IsNullOrEmpty(userName))
+                    return HttpStatusCode.OK;
+
+                response.Cookies.Add(GenerateCookie(userName, DateTime.Now.AddYears(-1)));
+
+                return response;
+            };
+
+            Get["account/current"] = parameters =>
+            {
+                var requestCookie = Request.Cookies.FirstOrDefault(c => c.Key == AUTH_COOKIE_NAME);
+                if (String.IsNullOrEmpty(requestCookie.Value))
+                    return HttpStatusCode.OK;
+
+                return requestCookie.Value;
+            };
+        }
+
+        private NancyCookie GenerateCookie(string userName, DateTime expiresAt)
+        {
+            return new NancyCookie(name: AUTH_COOKIE_NAME, value: userName, httpOnly: true) { Path = "/" };
+        }
+    }
+}
