6c8dd37ed288bccb8ec4409c3a475559c5050c62
Arthur <arthur.peka@outlook.com>
2015-10-25 23:37:19 +0200

    adding Subforum update test

--- Gorgosaurus.DA.IntegrationTests/CrudTest.cs
+++ Gorgosaurus.DA.IntegrationTests/CrudTest.cs
@@ -31,15 +31,40 @@ namespace Gorgosaurus.DA.IntegrationTests
                 Title = title,
                 Description = "desc 1"
             });
+
             var subforum = SubforumRepository.Instance.Get(1);
             Assert.True(subforum.Title.Equals(title));
+        }
+
+        [Test]
+        public void CanUpdateSubforum()
+        {
+            const string title = "Title";
+            const string newTitle = "Title 1";
+
+            var subforum = new Subforum()
+            {
+                Id = 1,
+                Title = title,
+                Description = "desc 1"
+            };
+
+            SubforumRepository.Instance.Insert(subforum);
+
+            subforum.Title = newTitle;
+
+            SubforumRepository.Instance.Update(subforum);
+
+            var dbSubforum = SubforumRepository.Instance.Get(1);
+
+            Assert.True(dbSubforum.Title.Equals(newTitle));
         }
 
 
         [TearDown]
         public void TearDown()
         {
-            DbConnector.Delete();
+            //DbConnector.Delete();
         }
     }
 }
