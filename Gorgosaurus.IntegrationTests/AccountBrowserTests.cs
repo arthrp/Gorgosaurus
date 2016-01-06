6622175017f75725273508fc7c4e258814a2b37b
Arthur <arthur.peka@outlook.com>
2016-01-06 17:55:04 +0200

    Login test

---
+++ Gorgosaurus.IntegrationTests/AccountBrowserTests.cs
@@ -0,0 +1,57 @@
+﻿using Gorgosaurus.BO.Entities;
+using Gorgosaurus.Common;
+using Gorgosaurus.DA;
+using Gorgosaurus.DA.Repositories;
+using Gorgosaurus.Models;
+using Nancy.Testing;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gorgosaurus.IntegrationTests
+{
+    [TestFixture]
+    public class AccountBrowserTests : BaseAccountTests
+    {
+        private const string USERNAME = "test";
+        private const string PASSWORD = "test";
+
+        [SetUp]
+        public void Setup()
+        {
+            DbConnector.Delete();
+            DbConnector.Init();
+
+            CreateUser(USERNAME, PASSWORD);
+        }
+
+        [Test]
+        public void CanLoginAndGetCurrentUser()
+        {
+            var bootstrapper = new GorgosaurusBootstrapper();
+            var browser = new Browser(bootstrapper, defaults: to => to.Accept("application/json"));
+
+            var resp = browser.Post("/account/login", with =>
+            {
+                with.HttpRequest();
+                with.JsonBody<LoginModel>( new LoginModel() { Username = USERNAME, Password = PASSWORD} );
+            });
+
+            Assert.True(resp.StatusCode == Nancy.HttpStatusCode.OK);
+            Assert.Greater(resp.Cookies.Count(), 0);
+
+            var authCookie = resp.Cookies.SingleOrDefault(c => c.Name == Constants.AUTH_COOKIE_NAME);
+            Assert.NotNull(authCookie);
+            Assert.True(!String.IsNullOrEmpty(authCookie.Value));
+
+            var userResp = browser.Get("/account/current", with => with.HttpRequest());
+
+            Assert.True(userResp.StatusCode == Nancy.HttpStatusCode.OK);
+            var returnedUsername = userResp.Body.AsString();
+            Assert.True(returnedUsername.Equals(USERNAME));
+        }
+    }
+}
---
+++ Gorgosaurus.IntegrationTests/BaseAccountTests.cs
@@ -0,0 +1,30 @@
+﻿using Gorgosaurus.BO.Entities;
+using Gorgosaurus.Common;
+using Gorgosaurus.DA.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gorgosaurus.IntegrationTests
+{
+    public abstract class BaseAccountTests
+    {
+        protected void CreateUser(string username, string password, long id = 1)
+        {
+            string userSalt = "11111";
+
+            string hash = CryptoHelper.GenerateHash(password, userSalt);
+            UserRepository.Instance.Insert(new ForumUser()
+            {
+                Id = id,
+                Username = username,
+                Password = userSalt + hash,
+                IsAdmin = true,
+                Name = "Tester",
+                Surname = "Testerson"
+            });
+        }
+    }
+}
