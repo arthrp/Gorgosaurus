edfed288125c9b6e00fcb88cd34651c513731047
Arthur <arthur.peka@outlook.com>
2016-01-11 23:03:22 +0200

    Starting Forum implementation

---
+++ Gorgosaurus/Modules/ForumModule.cs
@@ -0,0 +1,22 @@
+﻿using Gorgosaurus.DA.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gorgosaurus.Modules
+{
+    public class ForumModule : BaseModule
+    {
+        public ForumModule()
+        {
+            Get["/subforums"] = parameters =>
+            {
+                var subforums = SubforumRepository.Instance.GetAll();
+
+                return subforums;
+            };
+        }
+    }
+}
