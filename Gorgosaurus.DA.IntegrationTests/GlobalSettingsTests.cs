531c514fb6e6d0c1928b51ddb34a3330b5422b5a
Arthur <arthur.peka@outlook.com>
2016-01-14 00:36:38 +0200

    Adding test for global settings

---
+++ Gorgosaurus.DA.IntegrationTests/GlobalSettingsTests.cs
@@ -0,0 +1,35 @@
+﻿using Gorgosaurus.DA.Managers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gorgosaurus.DA.IntegrationTests
+{
+    [TestFixture]
+    public class GlobalSettingsTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            DbConnector.Delete();
+            DbConnector.Init();
+        }
+
+        [Test]
+        public void CanLoadAndSaveGlobalSetting()
+        {
+            const string newForumName = "Updated forum name";
+            var currentForumName = GlobalSettingsManager.Instance.Load(GlobalSettingsEnum.ForumName);
+
+            Assert.IsNotNullOrEmpty(currentForumName);
+
+            GlobalSettingsManager.Instance.Save(GlobalSettingsEnum.ForumName, newForumName);
+            var updatedName = GlobalSettingsManager.Instance.Load(GlobalSettingsEnum.ForumName);
+            Assert.IsNotNullOrEmpty(updatedName);
+            Assert.True(updatedName.Equals(newForumName));
+        }
+    }
+}
