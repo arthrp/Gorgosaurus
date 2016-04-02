cf9e685e3ff24851471ba38a628fcd45a612406d
Arthur <arthur.peka@outlook.com>
2016-04-03 01:29:01 +0300

    Fixing missing file

---
+++ Gorgosaurus/Content/Angular/subforum.controller.js
@@ -0,0 +1,61 @@
+﻿(function () {
+    angular.module('forumApp').controller('subforumController', subforumController);
+
+    subforumController.$inject = ['$uibModal', '$stateParams', '$http', '$scope', '$rootScope'];
+
+    function subforumController($uibModal, $stateParams, $http, $scope, $rootScope) {
+        var self = this;
+
+        self.current = null;
+        self.currentUsername = $rootScope.username;
+        self.pagesArr = [];
+        self.baseUrl = 'subforum/' + $stateParams['title'] + '/';
+        $scope.subforumTitle = $stateParams['title'];
+
+        self.addDiscussion = function () {
+            var modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'myModalContent.html',
+                controller: 'addDiscussionModal as addDiscussionCtrl',
+                size: 300,
+                resolve: {
+                    subforumId: function () {
+                        return self.current.id;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (res) {
+                if (res) {
+                    getSubforum();
+                }
+            });
+        };
+
+        self.getPageUrl = function (pageNumber) {
+            return self.baseUrl + '?page=' + pageNumber;
+        };
+
+        function getSubforum() {
+            var titleToSearchFor = $stateParams['title'];
+            var page = ($stateParams['page']) ? $stateParams['page'] : 0;
+
+
+            $http.get('/subforum/' + titleToSearchFor + '?page=' + page)
+                .success(function (res) {
+                    console.log('res',res);
+                    self.current = res;
+                    self.pagesArr = new Array(res.totalPages);
+
+                    console.log(self.pagesArr);
+                    $rootScope.pageTitle = self.current.title;
+                })
+                .error(function (res) {
+                    console.log(res);
+                    self.current = null;
+                });
+        }
+
+        getSubforum();
+    }
+})();
